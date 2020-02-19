1faed37fff005bccfaf48cefc6be8a9f3cb2d13f
Jens Trolle <jens@trolle.co>
2020-02-19 14:42:16 +0100

    fix på pairs(L)

--- uge_4/handin3.py
+++ uge_4/handin3.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Handin 3
-"""
-
-from random import randint
+'''
+HANDIN 3: Triplet Distance (Part 1)
 
 
-"""
-a)
-Make a function generate_labels(n), that given an integer n returns a list of n
-distinct strings, e.g. 'A', 'B', ... or "L1", 'L2' ...
-"""
+Afleveringen er lavet af:
+    201807850 (Jens Trolle)
+    201806389 (Mike Pedersen)
+    201805266 (Frederik Thaibert)
 
+Reflektion:
+
+'''
+
+# Opgave a)
+
+# Input af mængde af strenge
 x = int(input("Skriv her, hvor mange strenge du vil have: "))
 
+# Tjek for mængde større end 0
 while x <= 0:
     x = int(input("Du skal have over 0 strenge, prøv igen: "))
 
 
+# Definerer funktion der giver liste med labels vha. list comprehention.
+# Her er det vigtigt at pointere, at måden vi har definereret
+# generate_labels(n) på gør, at vi kan sammenligne elementerne,
+# f.eks. er 'L1' < 'L2'.
 def generate_labels(n):
     return [("L%s" % (i+1)) for i in range(n)]
 
 
+# Printer listen af x labels vha. generate_labels(n)
 print(generate_labels(x))
 
 
@@ -40,7 +49,14 @@ Note. Using the function shuffle from the module random to solve the question
 would be considered cheating.
 """
 
+from random import randint
 
+
+# Definerer funktionen permute(L), der permuterer en given liste. 
+# Vi gør dette på den måde der er beskrevet i hintet i opgavebeskrivelsen,
+# ved at starte med hele L, og så tage et tilfældigt element ud, putte det ind
+# i en ny liste. Derefter fjerner vi et element fra L og begynder igen, indtil
+# L er tom.
 def permute(L):
     new = []
     for i in range(0, len(L))[::-1]:
@@ -48,7 +64,7 @@ def permute(L):
         new.append(L.pop(b))
     return new
 
-
+# Printer en permutation af x labels, ved at bruge permute(generate_labels(n)).
 print(permute(generate_labels(x)))
 
 
@@ -59,10 +75,14 @@ returns a list of all pairs, i.e. tuples with two elements, (a, b) where a < b.
 """
 
 
+# Definerer funktionen pairs(L), der returnerer (a, b) for elementet a i L,
+# hvis der for b i L gælder, at a < b. Vi sorterer også elementerne for at
+# tage højde for, at vi f.eks. har 'L1' < 'L10' < 'L11' < 'L2'.
 def pairs(L):
-    return [(a, b) for a in L for b in L if a < b]
+    return sorted([(a, b) for a in L for b in L if a < b])
 
 
+# Printer parene der er i generate_labels(n).
 print(pairs(generate_labels(x)))
 
 
@@ -73,7 +93,8 @@ triples where the left subtree contains a label from A and the right subtree
 is a pair from B.
 """
 
-
+# Vi splitter vores liste af labels (generate_labels(x)) op i to lister. Hvis
+# x er ulige har 
 A1 = generate_labels(x)[:(x//2)]
 B1 = generate_labels(x)[(x//2):]
 
@@ -94,9 +115,10 @@ list L and the right subtree contains the labels in the list R.
 
 
 def anchored_triplets(L, R):
-    e = [(a, b) for a in R for b in pairs(L)]
-    e.extend(canonical_triplets(L, R))
+    e = canonical_triplets(L, R)
+    f = canonical_triplets(R, L)
+    e.extend(f)
     return e
 
 
-print(anchored_triplets(A1, B1))
+print(len(anchored_triplets(A1, B1)))
