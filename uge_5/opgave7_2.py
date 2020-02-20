1e28d6ad966ff58a41b6fa3c3c56d1195eeef532
Jens Trolle <jens@trolle.co>
2020-02-20 13:43:01 +0100

    7.1, 7.2 færdige

--- uge_5/opgave7_2.py
+++ uge_5/opgave7_2.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
 Exercise 7.2 (sum of squares)
+"""
 
+"""
 Make a function that can take any number of arguments, and returns the sum of
-the squares of the values, i.e. square_sum(x1,x2,x3) returns x12+ x22+ x32.
+the squares of the values, i.e. square_sum(x1,x2,x3) returns x1**2 + x2**2 +
+x3**2.
+"""
 
+
+def square_sum(*x):
+    return sum(a**2 for a in [*x])
+
+
+print(square_sum(1, 2, 3, 4))
+
+"""
 Given a list L, e.g. L = [1,2,3,4], use your square_sum function to compute
 the sum of squares of the values in L:
 
 Hint. Use a * to indicate an arbitrary argument list.
-
 """
+
+
+L = [1, 2, 3, 4]
+
+print(square_sum(*L))
