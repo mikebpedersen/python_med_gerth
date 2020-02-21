a0b78d52cfb8313cdbd3132ace8abb46e7b00509
Jens Trolle <jens@trolle.co>
2020-02-21 09:18:09 +0100

    Handin3 opdateret

--- uge_5/opgave8_1.py
+++ uge_5/opgave8_1.py
@@ -14,3 +14,20 @@ Hint. Use str.lower() and str.upper()to convert a string containing one letter
 to lower and upper case.
 
 """
+
+
+def binomial_A(n, k):
+    result = 1
+    for i in range(k):
+        result = result * (n - i) // (k - i)
+    return result
+
+
+def binomial_B(n, k):
+    result = 1
+    for i in range(k)[::-1]:
+        result = result * (n - i) // (k - i)
+    return result
+
+
+print(binomial_A(10, 5), binomial_B(10, 5))
